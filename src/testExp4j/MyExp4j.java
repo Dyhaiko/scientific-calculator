969e9959c5b68c6d47a631e26e1e70e99882c0f4
Dyhaiko <2605933016@qq.com>
2024-11-24 17:15:15 +0800

    Update MyExp4j.java

--- src/testExp4j/MyExp4j.java
+++ src/testExp4j/MyExp4j.java
@@ -2,4 +2,11 @@ package testExp4j;
 import net.objecthunter.exp4j.Expression;
 import net.objecthunter.exp4j.ExpressionBuilder;
 public class MyExp4j {
+    public static void main(String[] args)
+    {
+        Expression e=new ExpressionBuilder("3*sin(cos(x))").variable("x").build();
+        e.setVariable("x",Math.PI);
+        double result=e.evaluate();
+        System.out.println(result);
+    }
 }
